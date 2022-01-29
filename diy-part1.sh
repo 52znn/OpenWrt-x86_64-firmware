04cfca13901cac67d081b4485404fe5af8078726
52znn <42986378+52znn@users.noreply.github.com>
2022-01-30 02:59:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-rm -rf package/lean/luci-app-openvpn-server
-git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packages
+echo 'src-git openwrt-packages https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
