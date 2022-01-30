905900bb00057d605a8f9b773af2f2f8b5371ad8
52znn <42986378+52znn@users.noreply.github.com>
2022-01-30 20:30:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,3 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
-rm -rf package/lean/luci-app-openvpn-server
-rm -rf package/lean/luci-app-ddns
