b0b56e2f2cc12e269a0711140c1503235988cc77
52znn <42986378+52znn@users.noreply.github.com>
2022-01-30 20:30:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.0.2/g' package/base-files/files/bin/config_generate
+rm -rf package/lean/luci-app-openvpn-server
+rm -rf package/lean/luci-app-ddns
