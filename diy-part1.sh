289b64a93a0cdee817d0e9d580ef88bd4b878a6d
52znn <42986378+52znn@users.noreply.github.com>
2022-03-26 18:55:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
-echo 'src-git 52znn https://github.com/52znn/packages.git' >>feeds.conf.default
+echo 'src-git 52znn https://github.com/52znn/packages' >>feeds.conf.default
